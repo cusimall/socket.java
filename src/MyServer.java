9f3d7754b5472f11469d0c9b6c7bdea7c4018394
leminh11345 <cusimall11345@gmail.com>
2023-03-24 18:48:07 +0700

    socket with java

---
+++ src/MyServer.java
@@ -0,0 +1,39 @@
+package demosocket.src;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.ServerSocket;
+public class MyServer {
+    public static void main(String argv[]) throws Exception
+    {
+        String sentence_from_client;
+        String sentence_to_client;
+        
+        try (//Tạo socket server, chờ tại cổng '6543'
+        ServerSocket welcomeSocket = new ServerSocket(6543)) {
+            System.out.println("server dang chay");
+            while(true) {
+                //chờ yêu cầu từ client
+                Socket connectionSocket = welcomeSocket.accept();
+                
+                //Tạo input stream, nối tới Socket
+                BufferedReader inFromClient =
+                    new BufferedReader(new
+                        InputStreamReader(connectionSocket.getInputStream())); 
+                
+                //Tạo outputStream, nối tới socket
+                DataOutputStream outToClient =
+                    new DataOutputStream(connectionSocket.getOutputStream());
+                
+                //Đọc thông tin từ socket
+                sentence_from_client = inFromClient.readLine();
+                
+                sentence_to_client = sentence_from_client +" (Server accepted!)" + '\n';
+                //ghi dữ liệu ra socket
+                outToClient.writeBytes(sentence_to_client); 
+                return;
+            }
+        }
+        
+    }
+}
